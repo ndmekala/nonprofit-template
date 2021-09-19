3ec2bbf0be6cb3a0e086e49186a63196b86e6d2a
Nirmal Mekala <ndmekala@icloud.com>
2021-09-18 20:08:00 -0500

    polishing two pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,16 @@ import Section from "../components/section";
 
 const IndexPage = () => {
 
-  const twh1 = "font-serif text-5xl mt-6"
-  const twp = "font-serif text-3xl mt-2"
+  const twh1 = "font-serif font-bold text-4xl mt-6"
+  const twp = "text-2xl font-light mt-2 leading-snug"
 
   return (
     <Layout heroImage={hero} pageTitle="Home">
+        {/* Some of the text edited. */}
       <h1 className={twh1}>Our Vision</h1>
-      <p className={twp}>Zion Baptist Church is a Spirit led, transformed body of believers whose sole vision is to Glorify GOD in all we do... centered in love, peace, and grace; healing, evangelizing, making disciples thus creating a Christ -centered community" ACTS 2;42-47 ("The Beloved Community")</p>
+      <p className={twp}>Zion Baptist Church is a Spirit-led, transformed body of believers whose sole vision is to glorify God in all we do, centered in love, peace, and grace. Healing, evangelizing, making disciples, thus creating a Christ-centered community—the Beloved Community (Acts 2:42-47).</p>
       <h1 className={twh1}>Our Mission</h1>
-      <p className={twp}>Zion Baptist Church exists to be a center of Christ-like love and healing, to introduce people to Jesus Christ, helping them mature in faith and grow in service, equipping them to go to the world to spread the good news, making Disciples of Jesus.</p>
+      <p className={twp}>Zion Baptist Church exists to be a center of Christ-like love and healing, to introduce people to Jesus Christ, helping them mature in faith and grow in service, equipping them to go to the world to spread the good news, making disciples of Jesus.</p>
 
       {/* <Section title="Our Vision">
           <p>Zion Baptist Church is a Spirit led, transformed body of believers
--- src/pages/sermons.js
+++ src/pages/sermons.js
@@ -3,10 +3,17 @@ import Layout from '../components/layout'
 import hero from '../images/hero/stock-5.jpg';
 
 const Sermons = () => {
+
+  const twh1 = "font-serif font-bold text-4xl mt-6";
+  const twp = "text-2xl font-light mt-2 leading-snug";
+  const twa = "text-cadet hover:underline";
+  
     return (
         <Layout heroImage={hero} pageTitle="Sermons">
-            <h1 className="text-7xl">WATCH</h1>
-            <h1 className="text-7xl">LISTEN</h1>
+            <h1 className={twh1}>Watch</h1>
+            <p className={twp}>You can view our sermons on <a href="https://www.facebook.com/621elwood/" className={twa}>Facebook Live</a>. <em>Services are Sundays at 11 am.</em></p>
+            <h1 className={twh1}>Listen</h1>
+            <p className={twp}>You can also listen to our sermons on 89.9 FM <a href="" className={twa}>KMOJ</a>. <em>Sermons air Sundays at 9 am.</em></p>
         </Layout>
     )
 }

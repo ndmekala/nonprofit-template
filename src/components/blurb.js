3b7a03603eaadc03a0515d113cdacaad0b9f8824
Nirmal Mekala <ndmekala@icloud.com>
2021-11-11 17:49:47 -0600

    building out a flex content block

--- src/components/blurb.js
+++ src/components/blurb.js
@@ -3,15 +3,26 @@ import * as React from 'react';
 const Blurb = (props) =>  {
     // depending on if a var connected to index is even flex ordering
 
+    // const yo = <div>
+    //     <h1>Wowee!!</h1>
+    // </div>
+
+    // return (yo) < -- this is also valid btw…
+
     return (
-        <div>
-            <div className="my-4">
-                <h1 className="font-serif font-bold text-5xl text-sienna">
-                    {props.title}
-                </h1>
+        <div className="flex flex-wrap">
+            <div className ="w-64">
+                <img src={props.imgSrc} alt={props.title + ' image'}></img>
             </div>
-            <div className="my-4 text-justify text-sienna text-lg">
-                {props.children}
+            <div style={{minWidth: '30rem'}} className="p-4"> {/* Problem here = breaks @ diff pts */}
+                <div className="my-4">
+                    <h1 className="font-serif font-bold text-4xl text-sienna">
+                        {props.title}
+                    </h1>
+                </div>
+                <div className="my-4 text-justify text-sienna text-lg">
+                    {props.children}
+                </div>
             </div>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Section from "../components/section";
 import Blurb from "../components/blurb"
 
 const IndexPage = () => {
-
+  // this has got to be temporary…
   const twh1 = "font-serif font-bold text-4xl mt-6"
   const twp = "text-2xl font-light mt-2 leading-snug"
 
@@ -18,7 +18,7 @@ const IndexPage = () => {
       <p className={twp}>Zion Baptist Church exists to be a center of Christ-like love and healing, to introduce people to Jesus Christ, helping them mature in faith and grow in service, equipping them to go to the world to spread the good news, making disciples of Jesus.</p>
       <h1 className={twh1}>Services</h1>
       <p className={twp}>Our Sunday service time is 11 am, with Sunday school beginning at 10 am.</p>
-      <Blurb title="yes">Hi</Blurb>
+      <Blurb title="yes" imgSrc="https://media.istockphoto.com/vectors/defocused-abstract-blue-and-green-background-vector-id1044292966?k=20&m=1044292966&s=612x612&w=0&h=-GYgsBeO-EZ9PT2jr5RMgd3IWnYsK_R53o21SssGEq0=">Hi</Blurb>
       {/* <Section title="Our Vision">
           <p>Zion Baptist Church is a Spirit led, transformed body of believers
           whose sole vision is to Glorify GOD in all we do... centered in
